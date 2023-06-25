f819ba7c9edfc00ae070746647128a2e4728806d
KarloM7 <64904717+KarloM7@users.noreply.github.com>
2023-06-25 11:46:54 +0200

    Delete php directory

--- php/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servername = "localhost:3310";
-    $username = "root";
-    $password = "";
-    $basename = "projekt";
-    $dbc = mysqli_connect($servername, $username, $password, $basename) or die('Error connecting to MySQL server.' . mysqli_connect_error());
-    mysqli_set_charset($dbc, "utf8");
-?>
--- php/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <div class="container-fluid footer mt-1 p-4">
-        <div class="row">
-            <div class="col text-white">
-                <?php echo "Karlo Matoić kmatoic@tvz.hr " . date('Y')?>
-            </div>
-        </div>
-    </div>
-</footer>
